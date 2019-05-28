47375492955ca14753d8948d913aaedb2471c479
LIMr1209 <aaa1058169464@126.com>
2019-05-28 17:14:07 +0800

    redis://

--- AtguiguShop/settings.py
+++ AtguiguShop/settings.py
@@ -169,7 +169,7 @@ REST_FRAMEWORK_EXTENSIONS = {
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis//127.0.0.1:6379',
+        'LOCATION': 'redis://127.0.0.1:6379',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
             "PASSWORD": "",
